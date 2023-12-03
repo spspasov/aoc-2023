24f2802a66fbe524765881e2ea477f4cc9dedf2b
Svetoslav Spasov <me@spasov.dev>
2023-12-03 16:00:12 +0200

    First commit

---
+++ day-2.py
@@ -0,0 +1,36 @@
+import os
+
+rules: dict = {
+    'red': 0,
+    'green': 0,
+    'blue': 0,
+}
+
+def aoc_2():
+    with open('input-2.txt') as file:
+        games: list[str] = file.readlines()
+        result: int = 0
+
+        for draws in games:
+            draws: list[str] = draws.split(':')[1].split(';')
+
+            for draw in draws:
+                cubes: list[str] = draw.split(',')
+                for cube in cubes:
+                    cube_amount, color = cube.strip().split(' ')
+                    if rules[color] < int(cube_amount):
+                        rules[color] = int(cube_amount)
+
+            result += rules['red'] * rules['green'] * rules['blue']
+
+            # reset the rules
+            rules['red'] = 0
+            rules['green'] = 0
+            rules['blue'] = 0
+
+        print(result)
+        file.close()
+
+
+if __name__ == '__main__':
+    aoc_2()
---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import re
+
+
+def string2digit(string: str) -> str:
+    if string.isdigit():
+        return string
+
+    d = {
+        "zero": '0',
+        "nil": '0',
+        "one": '1',
+        "two": '2',
+        "three": '3',
+        "four": '4',
+        "five": '5',
+        "six": '6',
+        "seven": '7',
+        "eight": '8',
+        "nine": '9',
+        "ten": '10',
+    }
+
+    return d[string]
+
+
+def aoc_1():
+    with open('input-1.txt') as file:
+        lines = file.readlines()
+        sum: int = 0
+
+        for line in lines:
+            numbers: str = ''
+
+            spelled_numbers = "(^a(?=\s)|one|two|three|four|five|six|seven|eight|nine|ten|[0-9]+)"
+
+            cursor_numbers = []
+
+            for cursor in range(len(line)):
+                x = re.findall(spelled_numbers, line[cursor:])
+
+                # only add the array if there are any numbers present
+                if len(x) != 0:
+                    cursor_numbers.append(x)
+
+            sum += int(string2digit(cursor_numbers[0][0])[0] + string2digit(cursor_numbers[-1][-1])[-1])
+
+        print(sum)
+        file.close()
+
+
+if __name__ == '__main__':
+    aoc_1()
