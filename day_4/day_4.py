ee33b922e06c004dd4bf4de7eb3544b61dc92257
Svetoslav Spasov <me@spasov.dev>
2023-12-04 13:28:41 +0200

    Add solution for day 4 part 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,33 +1,61 @@
 def aoc_3():
     with open('input_4.txt') as file:
         cards: list[str] = file.readlines()
-        result: int = 0
+        won_cards: dict = {}
+        # won_cards = {
+        # 2: 5
+        # 3: 2
+        # ...
 
         for card in cards:
             card = card.strip()
             nums = card.split(':')[-1]
             winning_nums = nums.split('|')[0].split(' ')
+            game_id = card.split(':')[0].split(' ')[-1]
             my_nums = list(dict.fromkeys(nums.split('|')[-1].split(' ')))
 
-            c_win_nums = 0
-            game_result = 0
+            print(game_id)
 
-            for cur_my in my_nums:
-                if not cur_my.isdigit():
-                    continue
+            try:
+                try:
+                    iterations = won_cards[int(game_id)] + 1
+                # except UnboundLocalError as error:
+                except KeyError as error:
+                    iterations = 1
 
-                for cur_win in winning_nums:
-                    if cur_my == cur_win:
-                        if c_win_nums == 0:
-                            game_result += 1
-                            c_win_nums += 1
+                for i in range(0, iterations):
+
+                    cards_won = 0
+
+                    for cur_my in my_nums:
+                        if not cur_my.isdigit():
                             continue
-                        game_result *= 2
 
-            result += game_result
+                        for cur_win in winning_nums:
+                            if not cur_my.isdigit():
+                                continue
 
-        print(result)
+                            if cur_my == cur_win:
+                                cards_won += 1
+
+                    for i in range(int(game_id)+1, int(game_id)+cards_won+1):
+                        try:
+                            won_cards[i] = won_cards[i] + 1
+                        except KeyError as error:
+                            won_cards[i] = 1
+            except KeyError as error:
+                print('-------------------------------------')
+                print('game over')
+                print('-------------------------------------')
+                # game over, we calculate total scores
+                pass
+
+        print(won_cards)
+        sum = 0
+        for card_game in won_cards:
+            sum += won_cards[card_game]
         file.close()
+        print(sum + len(cards))
 
 
 if __name__ == '__main__':
