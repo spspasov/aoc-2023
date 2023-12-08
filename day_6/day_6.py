5fcce757bf9b5016f409f62fb3247fd81bf6f55d
Svetoslav Spasov <me@spasov.dev>
2023-12-09 00:29:07 +0200

    Optimize for early return

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,34 +1,15 @@
 def aoc_6():
     with open('input.txt') as file:
         lines = file.readlines()
+        time = lines[0].split(':')[1].strip().split(' ')[0]
+        record = lines[1].split(':')[1].strip().split(' ')[0]
 
-        times = lines[0].split(':')[1].strip().split(' ')
-        records = lines[1].split(':')[1].strip().split(' ')
-        distances = []
+        for duration in range(int(time.strip())+1):
+            distance = duration * (int(time) - duration)
 
-        for time in times:
-            race = []
-
-            if not time:
-                continue
-
-            for duration in range(int(time.strip())+1):
-                distance = duration * (int(time) - duration)
-                race.append(distance)
-
-            distances.append(race)
-
-        product = 1
-
-        for i, record in enumerate(records):
-            sum = 0
-            for distance in distances[i]:
-                if distance > int(record.strip()):
-                    sum += 1
-
-            product *= sum
-
-        print(product)
+            if distance > int(record):
+                print(int(time) - duration*2 + 1)
+                return 1
 
 
 if __name__ == '__main__':
