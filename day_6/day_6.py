00662f4f22e9ab975ebe2c0e8320da629e0d0991
Svetoslav Spasov <me@spasov.dev>
2023-12-09 00:57:01 +0200

    Minor optimization

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,15 +1,15 @@
 def aoc_6():
     with open('input.txt') as file:
         lines = file.readlines()
-        time = lines[0].split(':')[1].strip().split(' ')[0]
+        time = int(lines[0].split(':')[1].strip().split(' ')[0])
         record = lines[1].split(':')[1].strip().split(' ')[0]
 
-        for duration in range(int(time.strip())+1):
-            distance = duration * (int(time) - duration)
+        for duration in range(time):
+            distance = duration * (time - duration)
 
             if distance > int(record):
-                print(int(time) - duration*2 + 1)
-                return 1
+                print(time - duration*2 + 1)
+                return
 
 
 if __name__ == '__main__':
